4ab9d20ab8222576763280bacf6f886571e2af51
PHANIKC <31725605+PHANIKC@users.noreply.github.com>
2017-09-07 13:48:11 +0530

    Create TestName.java

---
+++ TestName.java
@@ -0,0 +1,23 @@
+//*******************************************************************
+// Dear CompileJava users,
+//
+// CompileJava has been operating since 2013 completely free. If you
+// find this site useful, or would otherwise like to contribute, then
+// please consider a donation (link in 'More Info' tab) to support
+// development of the new CompileJava website (stay tuned!).
+//
+// Most sincerely, Z.
+//*******************************************************************
+
+import java.lang.Math; // headers MUST be above the first class
+
+// one class needs to have a main() method
+public class TestName
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)
+  {
+    OtherClass myObject = new OtherClass("Hello World!");
+    System.out.print(myObject);
+  }
+}
